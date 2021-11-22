e169325c88f21ca50c8227a1e441afd7a5b9aa17
zTheDeer <91803387+zTheDeer@users.noreply.github.com>
2021-11-22 11:44:48 +0300

    Add files via upload

---
+++ m.py
@@ -0,0 +1,51 @@
+import time
+from pynput.keyboard import Controller
+from pynput.mouse import Button,Controller
+
+print("Mouse or Keyboard?")
+print("***********************")
+print("1 for Mouse")
+print("2 for Keyboard")
+choice = input("Choose: ")
+saniye = input("How many clicks on second?: ")
+saniye = float(saniye)
+while choice == "1" or "2":
+    if choice == "1":
+        mouse = Controller()
+        print('The current pointer position is {0}'.format(mouse.position))
+        question = input("Do you want to set a mouse position (Y/N): ")
+        if question.upper() == "Y":
+            x = input("X: ")
+            y = input("Y: ")
+            mouse.position = (x, y)
+            
+        elif question.upper() == "N":
+            pass
+        else:
+            print("Invalid Choice")
+        print("Right Button Or Left Button: ")
+        lr = input("L/R: ")
+        if lr.upper() == "L":
+            while True:
+                mouse.press(Button.left)
+                mouse.release(Button.left)
+                time.sleep(float(saniye))
+        elif lr.upper() == "R":
+            while True:
+                mouse.press(Button.Right)
+                mouse.release(Button.Right)
+                time.sleep(float(saniye))
+        else:
+            print("Invalid Choice")
+    if choice == "2":
+        tus = input("Which key do you want to press?: ")
+        keyboard = Controller()
+        while True:
+            keyboard.press(tus)
+            keyboard.release(tus)
+            time.sleep(float(saniye))
+    else:
+        print("Invalid Choice")
+
+
+
